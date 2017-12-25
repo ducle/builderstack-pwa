3634fd05daa0bde92a3d84a60d3bb90e03fd26f2
Le Duc <ducle.sg@gmail.com>
2017-12-25 16:12:45 +0700

    add action button next section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+const arrayNavigation = ['card', 'photo', 'features', 'map', 'contact'];
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,4 +11,9 @@ export class HomeComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+  nextSection() {
+    const index = arrayNavigation.findIndex(item => item === this.activeTab);
+    const nextIndex = arrayNavigation.length > index + 1 ? index + 1 : 0;
+    this.activeTab = arrayNavigation[nextIndex];
+  }
 }
