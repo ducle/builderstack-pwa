5bae6c7f5319048a91b0052bdd24e7f889affda6
Le Duc <ducle.sg@gmail.com>
2017-12-27 16:30:39 +0700

    add swiper active tab

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CardService } from '../services/card.service';
 
-const arrayNavigation = ['card', 'photo', 'features', 'map', 'contact'];
+const arrayNavigation = [ 0, 1, 2, 3, 4, 5];
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  activeTab = 'card';
+  activeTab = 0;
   isShowGallery: Boolean = false;
   SWIPE_ACTION = { LEFT: 'swipeleft', RIGHT: 'swiperight' };
   constructor(private cardService: CardService) {}
@@ -19,24 +19,24 @@ export class HomeComponent implements OnInit {
     // });
   }
   navigatonSection(next = true) {
-    const index = arrayNavigation.findIndex(item => item === this.activeTab);
+    const index = this.activeTab;
     let nextIndex = arrayNavigation.length > index + 1 ? index + 1 : 0;
     if (!next) {
       nextIndex = index - 1 >= 0 ? index - 1 : arrayNavigation.length - 1;
     }
-    this.activeTab = arrayNavigation[nextIndex];
+    this.activeTab = nextIndex;
   }
 
-  swipe(action = this.SWIPE_ACTION.RIGHT) {
-    if (this.isShowGallery) {
-      return;
-    }
-    if (action === 'swipedown') {
-      this.navigatonSection();
-    } else {
-      this.navigatonSection(false);
-    }
-  }
+  // swipe(action = this.SWIPE_ACTION.RIGHT) {
+  //   if (this.isShowGallery) {
+  //     return;
+  //   }
+  //   if (action === 'swipedown') {
+  //     this.navigatonSection();
+  //   } else {
+  //     this.navigatonSection(false);
+  //   }
+  // }
   callShowGalerry(status) {
     this.isShowGallery = status;
   }
