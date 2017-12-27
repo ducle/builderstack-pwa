f55330a1cd7f6b2c0cf79e92d2889d88d2b364d6
Le Duc <ducle.sg@gmail.com>
2017-12-27 12:01:38 +0700

    add gallery photo

--- src/app/home-photo/home-photo.component.ts
+++ src/app/home-photo/home-photo.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+const imageArray = [
+  'assets/images/one.jpg',
+  'assets/images/two.jpg',
+  'assets/images/three.jpg',
+  'assets/images/four.jpg',
+  'assets/images/five.jpg',
+  'assets/images/six.jpg'
+];
 @Component({
   selector: 'app-home-photo',
   templateUrl: './home-photo.component.html',
   styleUrls: ['./home-photo.component.scss']
 })
 export class HomePhotoComponent implements OnInit {
+  images = imageArray;
+  showGalerry: Boolean = false;
+  imageGalleryIndex: Number = 0;
+  myInterval = 0;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
