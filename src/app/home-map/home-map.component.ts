5413073f526bc77575cc093d722916ceb7832b35
Le Duc <ducle.sg@gmail.com>
2018-01-09 13:34:14 +0700

    fix map

--- src/app/home-map/home-map.component.ts
+++ src/app/home-map/home-map.component.ts
@@ -6,8 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-map.component.scss']
 })
 export class HomeMapComponent implements OnInit {
-  lat: Number = 51.678418;
-  lng: Number = 7.809007;
+  lat: Number = 42.956305;
+  lng: Number = -78.6916818;
+  zoom: Number = 12;
   constructor() {}
 
   ngOnInit() {}
