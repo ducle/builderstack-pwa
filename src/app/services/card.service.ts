d61fda5daa9ad91c714f395ed101bf2089132189
Le Duc <ducle.sg@gmail.com>
2018-01-26 10:41:40 +0700

    fix firebase changed

--- src/app/services/card.service.ts
+++ src/app/services/card.service.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database-deprecated';
+import {
+  AngularFireDatabase,
+  FirebaseObjectObservable,
+  FirebaseListObservable
+} from 'angularfire2/database-deprecated';
 
 @Injectable()
 export class CardService {
-  private dbPath = '/Cards';
+  private dbPath = 'Cards/Cards';
   card: FirebaseObjectObservable<any> = null;
+  cards: FirebaseListObservable<any[]> = null;
   constructor(private db: AngularFireDatabase) {}
 
   getCard(key): FirebaseObjectObservable<any> {
     this.card = this.db.object(`${this.dbPath}/${key}`);
     return this.card;
+  }
+  getCards(): FirebaseListObservable<any[]> {
+    this.cards = this.db.list(this.dbPath);
+    return this.cards;
   }
 }
