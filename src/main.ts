0afe06454629a31b5ed9768692103830fe3a761b
Le Duc <ducle.sg@gmail.com>
2018-02-01 14:14:04 +0700

    add service worker for firebase

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,13 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .then(() => {
+    try {
+      if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/ngsw-worker.js');
+      }
+    } catch (e) {}
+  })
   .catch(err => console.log(err));
